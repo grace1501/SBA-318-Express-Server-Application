dd933d5c94200227c2c3c08f44e0d8a8c219f699
Grace Do <61570746+grace1501@users.noreply.github.com>
2024-03-27 12:14:08 -0500

    fixed typo in users.js

--- data/users.js
+++ data/users.js
@@ -14,35 +14,35 @@ const users =
     "id": 2,
     "email": "janet.weaver@reqres.in",
     "first_name": "Janet",
-    "last_name": "Weaver",
+    "last_name": "Weaver"
 
     },
     {
     "id": 3,
     "email": "emma.wong@reqres.in",
     "first_name": "Emma",
-    "last_name": "Wong",
+    "last_name": "Wong"
     
     },
     {
     "id": 4,
     "email": "eve.holt@reqres.in",
     "first_name": "Eve",
-    "last_name": "Holt",
+    "last_name": "Holt"
     
     },
     {
     "id": 5,
     "email": "charles.morris@reqres.in",
     "first_name": "Charles",
-    "last_name": "Morris",
+    "last_name": "Morris"
     
     },
     {
     "id": 6,
     "email": "tracey.ramos@reqres.in",
     "first_name": "Tracey",
-    "last_name": "Ramos",
+    "last_name": "Ramos"
     
     }
     ]
